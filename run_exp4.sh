f970381cbe1ef875f981476ee8b76d1e5ee5bfbf
Philipp Mieden <dreadl0ck@protonmail.ch>
2018-11-21 11:39:46 +0100

    adjustments

--- run_exp4.sh
+++ run_exp4.sh
@@ -2,18 +2,18 @@
 
 NUM=4
 
-# echo "[INFO] running experiment $NUM"
-# echo "[INFO] parsing data"
-# netcap -r Tuesday-WorkingHours.pcapng -out Tuesday-WorkingHours-$NUM
-# netcap -r Wednesday-WorkingHours.pcapng -out Wednesday-WorkingHours-$NUM
-# netcap -r Thursday-WorkingHours.pcapng -out Thursday-WorkingHours-$NUM
-# netcap -r Friday-WorkingHours.pcapng -out Friday-WorkingHours-$NUM
+echo "[INFO] running experiment $NUM"
+echo "[INFO] parsing data"
+netcap -r Tuesday-WorkingHours.pcapng -out Tuesday-WorkingHours-$NUM
+netcap -r Wednesday-WorkingHours.pcapng -out Wednesday-WorkingHours-$NUM
+netcap -r Thursday-WorkingHours.pcapng -out Thursday-WorkingHours-$NUM
+netcap -r Friday-WorkingHours.pcapng -out Friday-WorkingHours-$NUM
 
-# echo "[INFO] labeling data"
-# netcap -r Tuesday-WorkingHours.pcapng -out Tuesday-WorkingHours-$NUM -label -description
-# netcap -r Wednesday-WorkingHours.pcapng -out Wednesday-WorkingHours-$NUM -label -description
-# netcap -r Thursday-WorkingHours.pcapng -out Thursday-WorkingHours-$NUM -label -description
-# netcap -r Friday-WorkingHours.pcapng -out Friday-WorkingHours-$NUM -label -description
+echo "[INFO] labeling data"
+netcap -r Tuesday-WorkingHours.pcapng -out Tuesday-WorkingHours-$NUM -label -description
+netcap -r Wednesday-WorkingHours.pcapng -out Wednesday-WorkingHours-$NUM -label -description
+netcap -r Thursday-WorkingHours.pcapng -out Thursday-WorkingHours-$NUM -label -description
+netcap -r Friday-WorkingHours.pcapng -out Friday-WorkingHours-$NUM -label -description
 
 echo "[INFO] evaluating"
 eval.sh Tuesday-WorkingHours-$NUM -dropna
--- stats.sh
+++ stats.sh
@@ -8,13 +8,14 @@ fi
 # remove trailing slash, if any
 INPUT_DIR=${1%/}
 
-echo "INPUT_DIR=$INPUT_DIR"
+echo -e "INPUT_DIR=$INPUT_DIR\n"
 
+printf "%-30s %-10s %-10s %-10s %-16s %s\n" "Filename" "Records" "Size" "Labels" "Time" "Score"
 for f in $(ls $INPUT_DIR/*_labeled.csv); do
 
 	res=${f%_labeled.csv}_RESULT.txt
 
-	recs=$(cat $res | grep 'rows.' | perl -pe 's/\x1b\[[0-9;]*m//g' | grep -o -E '[0-9]+' | tr '\n' ' ')
+	recs=$(cat $res | grep 'rows\.' | perl -pe 's/\x1b\[[0-9;]*m//g' | grep -o -E '[0-9]+' | tr '\n' ' ')
 
 	size=$(du -h $f | cut -f -1)
 	
@@ -23,12 +24,19 @@ for f in $(ls $INPUT_DIR/*_labeled.csv); do
 	labels_count=$(($labels-1))
 
 	time=$(cat $res | grep 'Time:')
-	time_n=${time#"Exec Time: "}
+	time_n=${time#"[INFO] Exec Time: "}
 	
 	score=$(cat $res | grep 'Validation score:')
-	score_n=${score#"Validation score: "}
+	score_n=${score#"[INFO] Validation score: "}
 
 	file=${f#"$INPUT_DIR/"}
+	
+	# echo "file=$file "
+	# echo "recs=$recs "
+	# echo "size=$size "
+	# echo "labels_count=$labels_count "
+	# echo "time_n=$time_n"
+	# echo "score_n=$score_n"
 
 	printf "%-30s %-10s %-10s %-10s %-25s %s\n" $file $recs $size $labels_count "$time_n" $score_n
 done
