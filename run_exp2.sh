ec84bea86f4ad25811a495bc0e3289e4a41965f5
Philipp Mieden <dreadl0ck@protonmail.ch>
2018-11-27 23:51:20 +0100

    reuse generated files if possible

--- run_exp2.sh
+++ run_exp2.sh
@@ -3,11 +3,29 @@
 NUM=2
 
 echo "[INFO] running experiment $NUM"
-echo "[INFO] parsing data"
-netcap -r Tuesday-WorkingHours.pcap -out Tuesday-WorkingHours-$NUM
-netcap -r Wednesday-WorkingHours.pcap -out Wednesday-WorkingHours-$NUM
-netcap -r Thursday-WorkingHours.pcap -out Thursday-WorkingHours-$NUM
-netcap -r Friday-WorkingHours.pcap -out Friday-WorkingHours-$NUM
+
+if [ -d "Tuesday-WorkingHours" ]
+then
+    echo "[INFO] using netcap data"
+
+    # make sure all folders for the current experiment exist
+    mkdir Tuesday-WorkingHours-$NUM
+    mkdir Wednesday-WorkingHours-$NUM
+    mkdir Thursday-WorkingHours-$NUM
+    mkdir Friday-WorkingHours-$NUM
+
+    # copy netcap data from previously generated folders to the current experiment folder
+    cp Tuesday-WorkingHours/*.ncap.gz Tuesday-WorkingHours-$NUM
+    cp Wednesday-WorkingHours/*.ncap.gz Wednesday-WorkingHours-$NUM
+    cp Thursday-WorkingHours/*.ncap.gz Thursday-WorkingHours-$NUM
+    cp Friday-WorkingHours/*.ncap.gz Friday-WorkingHours-$NUM
+else
+    echo "[INFO] parsing data"
+    netcap -r Tuesday-WorkingHours.pcap -out Tuesday-WorkingHours
+    netcap -r Wednesday-WorkingHours.pcap -out Wednesday-WorkingHours
+    netcap -r Thursday-WorkingHours.pcap -out Thursday-WorkingHours
+    netcap -r Friday-WorkingHours.pcap -out Friday-WorkingHours
+fi
 
 echo "[INFO] labeling data"
 netlabel -r Tuesday-WorkingHours.pcap -out Tuesday-WorkingHours-$NUM
